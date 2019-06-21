142c3c4d63923e3820866536cb88395775d51766
objectorange <objectorange@protonmail.com>
2019-06-21 14:24:25 -0400

    implement translator

--- src/main/java/io/onemfive/radio/signals/QTranslator.java
+++ src/main/java/io/onemfive/radio/signals/QTranslator.java
@@ -1,7 +1,50 @@
 package io.onemfive.radio.signals;
 
+import io.onemfive.data.util.FileUtil;
+
+import java.util.Map;
+import java.util.logging.Logger;
+
 /**
  * Translate QSignal into i18n/l10n
  */
 public class QTranslator {
+
+    private static Logger LOG = Logger.getLogger(QTranslator.class.getName());
+    private static Map<String, String> translations;
+
+    public static String translateQuery(QSignal querySignal, String ... args) {
+        loadTranslations();
+        return merge(translations.get(querySignal.name()+"-en-q"), args);
+    }
+
+    public static String translateResponse(QSignal responseSignal, String ... args) {
+        loadTranslations();
+        return merge(translations.get(responseSignal.name()+"-en-r"), args);
+    }
+
+    public static String translateQuery(String twoCharLang, QSignal querySignal, String ... args) {
+        loadTranslations();
+        return merge(translations.get(querySignal.name()+"-"+twoCharLang+"-q"), args);
+    }
+
+    public static String translateResponse(String twoCharLang, QSignal responseSignal, String ... args) {
+        loadTranslations();
+        return merge(translations.get(responseSignal.name()+"-"+twoCharLang+"-r"), args);
+    }
+
+    private static void loadTranslations() {
+        if(translations == null) {
+            translations = FileUtil.textFileToMap("translations.txt", 1000, "=", true, true);
+        }
+    }
+
+    private static String merge(String t, String ... args) {
+        int h = 1;
+        for(String a : args) {
+            t = t.replace(":"+(h++), a);
+        }
+        return t;
+    }
+
 }
