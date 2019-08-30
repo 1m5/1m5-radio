43d9e252feb691c9528835c2dd87510ad6a75afb
objectorange <objectorange@1m5.io>
2019-08-30 00:45:18 -0400

    allow for options

--- src/main/java/io/onemfive/radio/Radio.java
+++ src/main/java/io/onemfive/radio/Radio.java
@@ -1,9 +1,11 @@
 package io.onemfive.radio;
 
+import java.util.Properties;
+
 /**
  * Interface to use for all Radio calls.
  */
 public interface Radio {
 
-    int sendMessage(RadioDatagram message);
+    int sendMessage(RadioDatagram datagram, Properties options);
 }
