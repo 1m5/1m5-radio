dd4551a3604c54b1086b4f4535779e7260dd5d81
objectorange <objectorange@1m5.io>
2019-08-30 00:44:48 -0400

    allow for options

--- src/main/java/io/onemfive/radio/vendor/GNURadio.java
+++ src/main/java/io/onemfive/radio/vendor/GNURadio.java
@@ -2,10 +2,13 @@ package io.onemfive.radio.vendor;
 
 import io.onemfive.radio.Radio;
 import io.onemfive.radio.RadioDatagram;
+import io.onemfive.radio.RadioPeer;
+
+import java.util.Properties;
 
 public class GNURadio implements Radio {
 
-    public int sendMessage(RadioDatagram datagram) {
+    public int sendMessage(RadioDatagram datagram, Properties options) {
         return 0;
     }
 
